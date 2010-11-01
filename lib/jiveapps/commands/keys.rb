5af35cf88549841a78bfc0646474a2303f03054c
sbecker <becker.scott@gmail.com>
2010-11-01 22:13:07 +0000

    AppHosting Command Line Tools: Add keys:add and keys:list commands
    git-svn-id: http://svn.jiveland.com/svn/repos/jaf/apphosting/jiveapps/trunk@126165 c71dbd84-28fa-0310-abbe-db3d66cda40d

--- lib/jiveapps/commands/keys.rb
+++ lib/jiveapps/commands/keys.rb
@@ -1,6 +1,52 @@
 module Jiveapps::Command
   class Keys < Base
-    def add
+
+    # Lists uploaded SSH keys
+    def list
+      long = args.any? { |a| a == '--long' }
+      ssh_keys = jiveapps.keys
+      if ssh_keys.empty?
+        display "No keys for #{jiveapps.user}"
+      else
+        display "=== #{ssh_keys.size} key#{'s' if ssh_keys.size > 1} for #{jiveapps.user}"
+        ssh_keys.each do |ssh_key|
+          display long ? ssh_key['key'].strip : format_key_for_display(ssh_key['key'])
+        end
+      end
     end
+
+    # Uploads an SSH Key
+    # - args.first can either be a path to a key file or be nil. if nil, looks in default paths
+    def add
+      keyfile = find_key(args.first)
+      key = File.read(keyfile)
+
+      display "Uploading ssh public key #{keyfile}"
+      jiveapps.add_key(key)
+    end
+
+    private
+      # Finds a key in the specified path or in the default locations (~/.ssh/id_(r|d)sa.pub)
+      def find_key(path=nil)
+        if !path.nil? && path.length > 0
+          return path if File.exists? path
+          raise CommandFailed, "No ssh public key found in #{path}."
+        else
+          %w(rsa dsa).each do |key_type|
+            keyfile = "#{home_directory}/.ssh/id_#{key_type}.pub"
+            return keyfile if File.exists? keyfile
+          end
+          raise CommandFailed, "No ssh public key found in #{home_directory}/.ssh/id_[rd]sa.pub.  You may want to specify the full path to the keyfile."
+        end
+      end
+
+      # Formats an SSH key for display by trimming out the middle
+      # Example Output:
+      # ssh-rsa AAAAB3NzaC...Fyoke4MQ== pablo@jive
+      def format_key_for_display(key)
+        type, hex, local = key.strip.split(/\s/)
+        [type, hex[0,10] + '...' + hex[-10,10], local].join(' ')
+      end
+
   end
 end
---
+++ spec/commands/keys_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+
+module Jiveapps::Command
+  describe Keys do
+    before do
+      @keys = prepare_command(Keys)
+      @keys.jiveapps.stub!(:user).and_return('joe')
+
+      @key_list_response = [
+        {
+          "name"       => "foobar",
+          "created_at" => "2010-11-01T18:31:04Z",
+          "updated_at" => "2010-11-01T18:31:04Z",
+          "username"   => "testuser",
+          "id"         => 3,
+          "key"        => "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAp9AJD5QABmOcrkHm6SINuQkDefaR0MUrfgZ1Pxir3a4fM1fwa00dsUwbUaRuR7FEFD8n1E9WwDf8SwQTHtyZsJg09G9myNqUzkYXCmydN7oGr5IdVhRyv5ixcdiE0hj7dRnOJg2poSQ3Qi+Ka8SVJzF7nIw1YhuicHPSbNIFKi5s0D5a+nZb/E6MNGvhxoFCQX2IcNxaJMqhzy1ESwlixz45aT72mXYq0LIxTTpoTqma1HuKdRY8HxoREiivjmMQulYP+CxXFcMyV9kxTKIUZ/FXqlC6G5vSm3J4YScSatPOj9ID5HowpdlIx8F6y4p1/28r2tTl4CY40FFyoke4MQ== pablo@jive\n"
+        }
+      ]
+    end
+
+    it "adds a key from the default locations if no key filename is supplied" do
+      @keys.should_receive(:find_key).and_return('/home/joe/.ssh/id_rsa.pub')
+      File.should_receive(:read).with('/home/joe/.ssh/id_rsa.pub').and_return('ssh-rsa xyz')
+      @keys.jiveapps.should_receive(:add_key).with('ssh-rsa xyz')
+      @keys.should_receive(:display).with("Uploading ssh public key /home/joe/.ssh/id_rsa.pub")
+      @keys.add
+    end
+
+    it "adds a key from a specified keyfile path" do
+      @keys.stub!(:args).and_return(['/my/key.pub'])
+      @keys.should_receive(:find_key).with('/my/key.pub').and_return('/my/key.pub')
+      File.should_receive(:read).with('/my/key.pub').and_return('ssh-rsa xyz')
+      @keys.jiveapps.should_receive(:add_key).with('ssh-rsa xyz')
+      @keys.add
+    end
+
+    it "list keys, trimming the hex code for better display" do
+      @keys.jiveapps.should_receive(:keys).and_return(@key_list_response)
+      @keys.should_receive(:display).with('ssh-rsa AAAAB3NzaC...Fyoke4MQ== pablo@jive')
+      @keys.list
+    end
+
+    it "list keys showing the whole key hex with --long" do
+      @keys.stub!(:args).and_return(['--long'])
+      @keys.jiveapps.should_receive(:keys).and_return(@key_list_response)
+      @keys.should_receive(:display).with("ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAp9AJD5QABmOcrkHm6SINuQkDefaR0MUrfgZ1Pxir3a4fM1fwa00dsUwbUaRuR7FEFD8n1E9WwDf8SwQTHtyZsJg09G9myNqUzkYXCmydN7oGr5IdVhRyv5ixcdiE0hj7dRnOJg2poSQ3Qi+Ka8SVJzF7nIw1YhuicHPSbNIFKi5s0D5a+nZb/E6MNGvhxoFCQX2IcNxaJMqhzy1ESwlixz45aT72mXYq0LIxTTpoTqma1HuKdRY8HxoREiivjmMQulYP+CxXFcMyV9kxTKIUZ/FXqlC6G5vSm3J4YScSatPOj9ID5HowpdlIx8F6y4p1/28r2tTl4CY40FFyoke4MQ== pablo@jive")
+      @keys.list
+    end
+
+    context "key locating" do
+      before do
+        @keys.stub!(:home_directory).and_return('/home/joe')
+      end
+
+      it "finds the user's ssh key in ~/ssh/id_rsa.pub" do
+        File.should_receive(:exists?).with('/home/joe/.ssh/id_rsa.pub').and_return(true)
+        @keys.send(:find_key).should == '/home/joe/.ssh/id_rsa.pub'
+      end
+
+      it "finds the user's ssh key in ~/ssh/id_dsa.pub" do
+        File.should_receive(:exists?).with('/home/joe/.ssh/id_rsa.pub').and_return(false)
+        File.should_receive(:exists?).with('/home/joe/.ssh/id_dsa.pub').and_return(true)
+        @keys.send(:find_key).should == '/home/joe/.ssh/id_dsa.pub'
+      end
+
+      it "finds the user's ssh key in the specified path, /my/path/id_rsa.pub" do
+        File.should_receive(:exists?).with('/my/path/id_rsa.pub').and_return(true)
+        @keys.send(:find_key, '/my/path/id_rsa.pub').should == '/my/path/id_rsa.pub'
+      end
+
+      it "raises an exception if neither id_rsa or id_dsa were found" do
+        File.stub!(:exists?).and_return(false)
+        lambda { @keys.send(:find_key) }.should raise_error(Jiveapps::Command::CommandFailed)
+      end
+
+      it "raises an exception if specified path not found" do
+        File.stub!(:exists?).and_return(false)
+        lambda { @keys.send(:find_key, '/my/path/id_rsa.pub') }.should raise_error(Jiveapps::Command::CommandFailed)
+      end
+    end
+
+  end
+end
