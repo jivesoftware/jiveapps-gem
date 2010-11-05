11749ac2b1f7bbf5f1768346f1ae4e05095bb469
sbecker <becker.scott@gmail.com>
2010-11-05 00:14:44 +0000

    AppHosting Command Line Tools: Update the hostname for the backend to be apphosting.jivesoftware.com
    git-svn-id: http://svn.jiveland.com/svn/repos/jaf/apphosting/jiveapps/trunk@126695 c71dbd84-28fa-0310-abbe-db3d66cda40d

--- lib/jiveapps.rb
+++ lib/jiveapps.rb
@@ -1,9 +1,7 @@
 module Jiveapps
   VERSION = '0.0.1'
-  # WEBHOST = 'ec2-75-101-212-31.compute-1.amazonaws.com'
-  # GITHOST = 'ec2-75-101-212-31.compute-1.amazonaws.com'
-  WEBHOST = 'becker-mbp.jiveland.com:3000'
-  GITHOST = 'becker-mbp.jiveland.com'
+  WEBHOST = 'apphosting.jivesoftware.com'
+  GITHOST = 'apphosting.jivesoftware.com'
 end
 
 require 'jiveapps/client'
