e1c2314f544df1355871fb32aca64c3600a808b7
Scott Becker <scott.becker@jivesoftware.com>
2011-07-29 10:22:56 -0700

    Update app template to use preferred height of 400 px on home view and not run gadgets.window.adjustHeight onload by default

--- app_generators/create/templates/javascripts/main.js
+++ app_generators/create/templates/javascripts/main.js
@@ -2,5 +2,5 @@ gadgets.util.registerOnLoadHandler(function() {
   // add code that should run on page load here
 
   // resize app window to fit content
-  gadgets.window.adjustHeight();
+  // gadgets.window.adjustHeight();
 });
